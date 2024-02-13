cdd67cd4b76ffcb615b691199df475e8678de160
Okaga64 <127199665+Okaga-AM@users.noreply.github.com>
2024-02-13 17:00:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var i = 0;
-    var messages = [
-      "Happy Valentine's Day Baby💘",
-      "Thank you for making everyday..",
-      "of my Life Memorable.. ",
-      "and Special🌹💕",
-      "Thank you for Caring about me..",
-      "the way you do☺️",
-      "Words can't describe...",
-      "how sweet you are to me🥰"
-    ];
-    var currentMessage = 0;
+    var txt = "Happy Valentine's Day Baby 💌💘🥀. Thank you for making everyday of my Life Memorable and Special 🌹💕. Thank you for Caring about me the way you do☺️, Words can't describe how sweet you are to me🥰";
     var speed = 100; // The speed/duration of the effect in milliseconds
   
     function typeWriter() {
-        if (i < messages[currentMessage].length) {
-          document.getElementById("demo").textContent += messages[currentMessage].charAt(i);
-          i++;
-          setTimeout(typeWriter, speed);
-        } else {
-          setTimeout(function() {
-            i = 0;
-            currentMessage++;
-            document.getElementById("demo").textContent = ''; // Clear the text
-            if (currentMessage < messages.length) {
-              setTimeout(typeWriter, 1500); // Pause for 1.5 seconds before starting the next message
-            } else {
-              var cursor = document.querySelector('.cursor');
-              if (cursor) {
-                cursor.style.display = 'none';
-              }
-            }
-          }, 200);
-        }
+      if (i < txt.length) {
+        document.getElementById("typewriter").textContent += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
       }
+    }
+  
     typeWriter();
   });
